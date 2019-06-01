522c5748f63a1645b55b1629ceb7eed283609848
Ouajib Aissam <ouajibaissam@gmail.com>
2019-06-01 01:43:40 +0000

    CURD operations all woriking for the localhost server

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:php_myadmin_demo/add_update_page.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
@@ -19,23 +20,6 @@ class _MyHomePageState extends State<MyHomePage> {
   var d;
   PersistentBottomSheetController t;
 
-  final formKey = GlobalKey<FormState>();
-  String _title;
-  String _subtitle;
-
-  _validateAndSave(){
-    final form = formKey.currentState;
-    if(form.validate()){
-      form.save();
-      _insertData();
-    } 
-  }
-
-  _insertData(){
-    print(_title);
-    print(_subtitle);
-  }
-
 
   @override
   void initState() { 
@@ -50,56 +34,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return data;
   }
 
-  _buildTextField(hint, label){
-    return TextFormField(
-      maxLength: 255,
-      decoration: InputDecoration(
-        hintText: hint,
-        labelText: label,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        ),
-      ),
-      validator: (value) => value.isEmpty ? "$label can't be empty!!" : null,
-      onSaved: (newValue){
-        if(label == 'Title') _title = newValue;
-        else _subtitle = newValue;
-      },
-    );
-  }
-
-  _buildForm(){
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: <Widget>[
-            _buildTextField('enter title...', 'Title'),
-            _buildTextField('enter subtitle...', 'Subtitle'),
-            RaisedButton(
-              color: Colors.blue,
-              child: Text('Submit', style: TextStyle(color: Colors.white),),
-              onPressed: () => _validateAndSave(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _buildBottomSeet(context){
-    t = showBottomSheet(
-      context: context,
-      builder: (ctx){
-        return Container(
-          height: 300,
-          width: double.infinity,
-          color: Colors.grey[200],
-          child: _buildForm(),
-        );
-      }
-    );
+  _deleteData(id){
+    String url = 'http://localhost/phpWithFlutter/delete.php';
+    http.post(url,body: {
+      'id': id,
+    }).then((_){
+      setState(() {});
+    });
   }
 
   @override
@@ -108,54 +49,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('phpMyAdmin Demo'),
       ),
-      body: Stack(
-        children: <Widget>[
-          FutureBuilder(
-            future: _getData(),
-            builder: (context, snap){
-              if(snap.hasError) return Text('error');
-              else if(!snap.hasData) return Text('loading');
-              return ListView.builder(
-                itemCount: snap.data.length,
-                itemBuilder: (context, i){
-                  return ListTile(
-                    title: Text(snap.data[i]['title']),
-                    subtitle: Text(snap.data[i]['subtitle']),
-                  );
-                },
+      body: FutureBuilder(
+        future: _getData(),
+        builder: (context, snap){
+          if(snap.hasError) return Text('error');
+          else if(!snap.hasData) return Text('loading');
+          return ListView.builder(
+            itemCount: snap.data.length,
+            itemBuilder: (context, i){
+              return ListTile(
+                title: Text(snap.data[i]['title']),
+                subtitle: Row(
+                  children: <Widget>[
+                    Text(snap.data[i]['subtitle']),
+                    Expanded(child: Container(),),
+                    IconButton(
+                      icon: Icon(Icons.edit, color: Colors.blue,),
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context)=> AddDataPage(id: snap.data[i]['id']))
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red,),
+                      onPressed: () => _deleteData(snap.data[i]['id']),
+                    ),
+                  ],
+                ),
               );
             },
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Builder(
-                builder: (context) {
-                  return FloatingActionButton(
-                    tooltip: 'Add',
-                    child: Icon(Icons.add),
-                    onPressed: () {
-                      _buildBottomSeet(context);
-                    },
-                  );
-                }
-              ),
-            ),
-          ),
-        ],
+          );
+        },
       ),
-      // floatingActionButton: Builder(
-      //   builder: (context) {
-      //     return FloatingActionButton(
-      //       tooltip: 'Add',
-      //       child: Icon(Icons.add),
-      //       onPressed: () {
-      //         _buildBottomSeet(context);
-      //       },
-      //     );
-      //   }
-      // ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=> AddDataPage())
+        ),
+      ),
     );
   }
 }
